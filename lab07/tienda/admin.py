562e9d48abcd64fb914fd9bf9d3b4cc04135f1c7
mvillanueva24 <mariorvillanueva24@gmail.com>
2021-10-27 18:16:18 -0500

    lab10

--- lab07/tienda/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Categoria, Producto
-
-admin.site.register(Categoria)
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('categoria', 'nombre')
-admin.site.register(Producto, ProductAdmin)
--- lab08/series/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.serie_list),
-    path('<int:pk>',views.serie_detail)
-]
